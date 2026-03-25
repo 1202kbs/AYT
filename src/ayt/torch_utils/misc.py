7450989026defdbde4e323dd8f5a2cdc9446b5a1
1202kbs <1202kbs@gmail.com>
2026-03-25 19:33:38 +0900

    Fix minor bug in package name

--- src/ayt/torch_utils/misc.py
+++ src/ayt/torch_utils/misc.py
@@ -10,7 +10,7 @@ import contextlib
 import numpy as np
 import torch
 import warnings
-from dada import dnnlib
+from ayt import dnnlib
 
 #----------------------------------------------------------------------------
 # Cached construction of constant tensors. Avoids CPU=>GPU copy when the
--- src/ayt/torch_utils/persistence.py
+++ src/ayt/torch_utils/persistence.py
@@ -19,7 +19,7 @@ import inspect
 import copy
 import uuid
 import types
-from dada import dnnlib
+from ayt import dnnlib
 
 #----------------------------------------------------------------------------
 
